2bca5eaaaf7ede13d98b67a607d85c52c1dd1467
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-14 23:53:18 +0500

    Working....

--- 01-Hello-World/index.js
+++ 01-Hello-World/index.js
@@ -19,13 +19,13 @@ const port = 3000
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello With the new!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-console.log("Hello World With the new")
+console.log("Hello Me With the new World")
 
 
 
@@ -66,3 +66,24 @@ console.log("Hello World With the new")
 //   });
 // });
 
+// // get single user
+// app.get("/user/:id", (req, res) => {
+//   const { id } = req.params;
+
+//   const index = users.findIndex((item) => item.id === +id);
+
+//   if (index === -1) {
+//     res.status(404).json({
+//       message: "user not found",
+//     });
+//     return;
+//   }
+
+//   res.status(200).json({
+//     data: users[index],
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
