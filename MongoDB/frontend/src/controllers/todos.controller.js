343622b45f045d51dd20692b4f39ad7ac68427f6
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-21 01:28:41 +0500

    Working..

--- MongoDB/frontend/src/controllers/todos.controller.js
+++ MongoDB/frontend/src/controllers/todos.controller.js
@@ -3,3 +3,13 @@ import Todos from "../models/todos.model.js"
 
 
 // add todo
+
+const  addTodo = (req , res ) =>{
+    const {title  , description} = req.body;
+
+    if (!title || !description) {
+        res.status(400).json({
+            message:"Title or descirption both required",mongodb+srv://muhammadsubhan0712:<db_password>@cluster0.7nasb.mongodb.net/
+        })
+    }
+}
