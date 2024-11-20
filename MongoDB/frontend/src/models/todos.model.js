37028495e081a82b82ed90dc03fc13ccb34dcfd5
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-21 01:25:17 +0500

    Working..

--- MongoDB/frontend/src/models/todos.model.js
+++ MongoDB/frontend/src/models/todos.model.js
@@ -4,19 +4,19 @@ import { type } from "os";
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        description:{
-            type: String,
-            required:true,
-        },
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps:true,
-    }
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-export default mongoose.model("Todos" ,todoSchema)
+export default mongoose.model("Todos", todoSchema);
