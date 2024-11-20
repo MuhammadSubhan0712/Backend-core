197bdaa0796e5bad253441ae30e11adc267261d3
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-21 02:23:47 +0500

    Done..

--- MongoDB/frontend/index.js
+++ MongoDB/frontend/index.js
@@ -1,27 +1,29 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
 
 import connectDB from "./src/db/index.js";
-import todosRoutes from  "./src/routes/todos.routes.js"
+import todosRoutes from "./src/routes/todos.routes.js";
+import { error } from "console";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/" , (req , res ) => {
-    res.send("Hello MongoDB");
+app.get("/", (req, res) => {
+  res.send("Hello MongoDB");
 });
 
-
 // routes
-app.use("/api/v1" , todosRoutes);
+app.use("/api/v1", todosRoutes);
 
 connectDB()
-.then(() =>{
-    app.listen(process.env.PORT, () =>{
-        console.log(`Server is running at port: ${process.env.PORT}`);;
-        
-    })
-})
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((erorr) => {
+    console.log("Mongo DB connection failed !!! ", error);
+  });
