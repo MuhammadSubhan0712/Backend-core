5f2d6fcf57d418426dbf55fa52959f42cd6b62ca
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-21 01:25:08 +0500

    Working..

---
+++ MongoDB/frontend/src/controllers/todos.controller.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { todo } from "node:test";
+// import Todos drom
+
+
+// add todo
+
+
+
---
+++ MongoDB/frontend/src/db/index.js
@@ -0,0 +1,15 @@
+
+import { log } from "console";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`
+        ${process.env.MONGO_URI}practice`);
+        console.log(`\n MOngoDB connected !! DB HOST: ${connectionInstance.connection.host}`);        
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+    }
+}
+
+export default connectDB;
