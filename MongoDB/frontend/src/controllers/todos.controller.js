54f3c7a9b8ae3d35a8a63dec70daa9c5060ceb15
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-21 01:30:49 +0500

    Working...

--- MongoDB/frontend/src/controllers/todos.controller.js
+++ MongoDB/frontend/src/controllers/todos.controller.js
@@ -1,24 +1,25 @@
 import mongoose from "mongoose";
-import Todos from "../models/todos.model.js"
-
+import Todos from "../models/todos.model.js";
 
 // add todo
 
-const  addTodo = (req , res ) =>{
-    const {title  , description} = req.body;
+const addTodo = (req, res) => {
+  const { title, description } = req.body;
 
-    if (!title || !description) {
-        res.status(400).json({
-            message:"Title or descirption both required",
-        })
-        return
-    }
-
-    const todo = Todos.create({
-        title , 
-        description,
+  if (!title || !description) {
+    res.status(400).json({
+      message: "Title or descirption both required",
     });
-    res.status(201).json({
-        message:"Todo added to the database successfully",
-    })
-}
+    return;
+  }
+
+  const todo = Todos.create({
+    title,
+    description,
+  });
+  res.status(201).json({
+    message: "Todo added to the database successfully",
+  });
+};
+
+export { addTodo };
