01f9036dd9adf564809455d38381790a5e80807f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-30 01:20:25 +0500

    First Commit

--- MongoDB/server/index.js
+++ MongoDB/server/index.js
@@ -3,11 +3,11 @@ dotenv.config();
 import express from "express";
 import connectDB from "./src/db/index.js";
 import todosRoutes from "./src/routes/todos.routes.js";
-
+import cors from "cors"
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello MongoDB");
 });
