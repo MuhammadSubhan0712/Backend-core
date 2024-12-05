4c4dc9e49bd6cd4efff76232c4857850f4cd10b3
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-05 23:25:42 +0500

    Working..

--- Authentication/src/controllers/user.controllers.js
+++ Authentication/src/controllers/user.controllers.js
@@ -1,13 +1,28 @@
 import User from "../models/user.models.js"
-
 import jwt from "jsonwebtoken"
 
+
+// To generate access token
 const generateAccessToken = (user) =>{
     return jwt.sign({email: user.email} , process.env.ACCESS_JWT_SECRET , {expiresIn: '6h'});   
 }
+
+
+// To generate refresh token
 
 const generateRefreshToken = (user) =>{
     return jwt.sign({email: user.email} , process.env.REFRESH_JWT_SECRET , {expiresIn: '7d'});   
 }
 
-
+// To register the User
+const registerUser = (user) =>{
+    
+}
+// To Login User
+const loginUser = (user) =>{
+    
+}
+// To logout User
+const logoutUser = (user) =>{
+    
+}
