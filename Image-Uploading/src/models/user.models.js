24a96b4d9254281cee5b632a8b90221d2a60b2f6
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-08 15:23:05 +0500

    Working..

---
+++ Image-Uploading/src/models/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UsersSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
+
+UsersSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+export default mongoose.model("Users", UsersSchema);
