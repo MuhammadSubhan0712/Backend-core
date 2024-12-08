78af037c5242ea7fcec70b139822098f00c48bbd
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-08 16:16:44 +0500

    Working..

--- Image-Uploading/src/controllers/user.controllers.js
+++ Image-Uploading/src/controllers/user.controllers.js
@@ -4,13 +4,15 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+
 // cloudinary configuration
-cloudinary.config(
-  (cloud_name = process.env.CLOUD_NAME),
-  (api_key = process.env.API_KEY),
-  (api_secret = process.env.API_SECRET), // # Click 'View API Keys' above to copy your API secret
-  (secure = True)
-);
+cloudinary.config({
+  cloud_name : process.env.CLOUD_NAME,
+  api_key : process.env.API_KEY,
+  api_secret : process.env.API_SECRET,
+});
+
+
 // Upload an image
 const uploadImageToCloudinary = async (localpath) => {
   try {
