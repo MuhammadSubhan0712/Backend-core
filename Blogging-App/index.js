56a9c51b69029a4b1942c14869c06da18ee42b41
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-09 22:27:43 +0500

    Working..

---
+++ Blogging-App/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/" , (req , res ) => {
+    res.send("Hello For Blogs");
+});
+
+app.listen(process.env.PORT , () =>{
+    console.log(` Server is running at port: ${process.env.PORT}`);
+
+});
