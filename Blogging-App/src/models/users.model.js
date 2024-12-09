48b3a24c433ef21b61a085bf39c784a7c2626463
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-09 23:10:55 +0500

    Done..

--- Blogging-App/src/models/users.model.js
+++ Blogging-App/src/models/users.model.js
@@ -1,25 +1,27 @@
-import mongoose, { mongo }  from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:[true | "username is required"],
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true | "username is required"],
     },
-    fullname:{
-        type:String,
-        required:[true | "full name is required"],
+    fullname: {
+      type: String,
+      required: [true | "full name is required"],
     },
-    email:{
-        type:String,
-        required:[true | "email is required"],
+    email: {
+      type: String,
+      required: [true | "email is required"],
     },
-    password:{
-        type:String,
-        required:[true | "password is required"],
-    }
-},
-{
-    timestamps: true
-});
+    password: {
+      type: String,
+      required: [true | "password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.model("Users",userSchema);
+export default mongoose.model("Users", userSchema);
