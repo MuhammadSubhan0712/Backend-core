8a638e67a9e8560da1469de28517821411d4d7a3
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-09 23:10:44 +0500

    Working..

--- Blogging-App/src/models/users.model.js
+++ Blogging-App/src/models/users.model.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true | "password is required"],
     }
+},
+{
+    timestamps: true
 });
 
 export default mongoose.model("Users",userSchema);
