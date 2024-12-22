4ce6479fee2e6c9f3f3d16890e734a417be2d1d0
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-23 00:22:57 +0500

    Blogging app server

--- Socket-io/server/index.js
+++ Socket-io/server/index.js
@@ -2,7 +2,6 @@ import express from "express"
 import http from "http"
 import { Server } from "socket.io"
 
-
 const app = express();
 
 const server = http.createServer(app);
@@ -15,5 +14,19 @@ const io  = new Server (server , {
 });
 
 io.on("connection" , (socket) => {
+    console.log("user connected: ",socket.id);
+
+
+socket.on("disconnect" , () => {
+    console.log("user disconnected");
     
-})
+});
+});
+
+app.get("/" , (req , res) => {
+    res.send("<h1>HeLLo sOckeT IO</h1>");
+});
+
+server.listen(3000 , () => {
+    console.log("listening on * : 3000");
+});
