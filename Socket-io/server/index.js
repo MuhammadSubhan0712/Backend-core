c3ccaf3f0f43a7943fea3d4dc4bd720d51cf5f36
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-23 10:24:36 +0500

    Working...

--- Socket-io/server/index.js
+++ Socket-io/server/index.js
@@ -1,32 +1,30 @@
-import express from "express"
-import http from "http"
-import { Server } from "socket.io"
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
 const server = http.createServer(app);
 
-const io  = new Server (server , {
-    cors: { 
-        origin: "http://localhost:5500",
-        methods: ["GET" , "POST"],
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5500",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on("connection" , (socket) => {
-    console.log("user connected: ",socket.id);
+io.on("connection", (socket) => {
+  console.log("user connected: ", socket.id);
 
-
-socket.on("disconnect" , () => {
+  socket.on("disconnect", () => {
     console.log("user disconnected");
-    
-});
-});
-
-app.get("/" , (req , res) => {
-    res.send("<h1>HeLLo sOckeT IO</h1>");
+  });
 });
 
-server.listen(3000 , () => {
-    console.log("listening on * : 3000");
+app.get("/", (req, res) => {
+  res.send("<h1>HeLLo sOckeT IO</h1>");
+});
+
+server.listen(3000, () => {
+  console.log("listening on * : 3000");
 });
