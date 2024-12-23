5427a6f5372fb2f2db68fc2d9dd31876ec019aba
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-24 00:35:30 +0500

    Done.

--- Blogging-App/src/routes/blogs.route.js
+++ Blogging-App/src/routes/blogs.route.js
@@ -1,10 +1,29 @@
-import express from "express"
+import express from "express";
 
-import { addBlog } from "../controllers/blogs.controllers.js"
+import {
+  addBlog,
+  getAllBlogs,
+  getSingleBlog,
+  deleteSingleBlog,
+  deleteSingleBlog,
+  editSingleBlog,
+} from "../controllers/blogs.controllers.js";
 
 const router = express.Router();
 
-router.post("/blog" , addBlog);
+// To post blog
+router.post("/blog", addBlog);
 
+// To get all blogs
+router.get("/blogs", getAllBlogs);
+
+// To get single blog
+router.get("/blog/:id", getSingleBlog);
+
+// To delete single blog
+router.delete("/blog/:id", deleteSingleBlog);
+
+// To edit single blog
+router.put("/blog/:id", editSingleBlog);
 
 export default router;
