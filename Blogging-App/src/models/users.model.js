efa31b1d7baeab682bb35e129da2ae6cb3f443fb
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-25 00:17:06 +0500

    Done.

--- Blogging-App/src/models/users.model.js
+++ Blogging-App/src/models/users.model.js
@@ -25,6 +25,7 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// ------------------------------------------------
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
