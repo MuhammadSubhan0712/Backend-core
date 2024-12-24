2bfa519ec319b0cee02ce60adc48e750c66e1060
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-25 00:35:09 +0500

    Working...

--- Blogging-App/src/routes/users.route.js
+++ Blogging-App/src/routes/users.route.js
@@ -1,3 +1,12 @@
-import express from "express"
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshTokens,
+} from "../controllers/users.controllers.js";
 
+const router = express.Router();
 
+// To register User
+router.post("/register", registerUser);
