3777a05072ee8bcefb8f976fe0c29e0e48135aa5
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-25 00:35:58 +0500

    Working..

--- Blogging-App/src/routes/users.route.js
+++ Blogging-App/src/routes/users.route.js
@@ -10,3 +10,14 @@ const router = express.Router();
 
 // To register User
 router.post("/register", registerUser);
+
+// To login User
+router.post("/login", loginUser);
+
+// To logout User
+router.post("/logout", logoutUser);
+
+// To refresh token
+router.post("/refreshtoken", refreshTokens);
+
+export default router;
