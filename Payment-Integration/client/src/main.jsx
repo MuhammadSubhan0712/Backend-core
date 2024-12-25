b873de50ab01febf36cd568320c5a19e2dd62d0f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-26 01:28:58 +0500

    routing done.

--- Payment-Integration/client/src/main.jsx
+++ Payment-Integration/client/src/main.jsx
@@ -1,23 +1,37 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter , RouterProvider } from "react-router-dom";
-import Layout from './Layout';
-import Home from './Home';
-const router = createBrowserRouter([{
-  path:"/",
-  element: <Layout/>,
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import Success from "./Component/Success";
+import Cancel from "./Component/Cancel";
 
-  children:[{
-    path:"/",
-    element:<Home/>,
-  },
+const router = createBrowserRouter([
   {
-    path:"success",
-    element:</>
-  },{}
-]
-}])
+    path: "/",
+    element: <Layout />,
 
-createRoot(document.getElementById('root')).render(
-<RouterProvider router={router} />
-)
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "success",
+        element: <Success />,
+      },
+      {
+        path: "cancel",
+        element: <Cancel />,
+      },
+      {
+        path: "*",
+        element: <h2>404 | Not Found </h2>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
