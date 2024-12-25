6d6939afc47b1e07f3db61a8d28f095445bf6434
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-26 01:14:30 +0500

    First Commit

---
+++ Payment-Integration/client/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import createBrowserRouter from "browser"
+const router = createBrowserRouter = [
+  {
+    <Layout/>
+  }
+]
+
+createRoot(document.getElementById('root')).render(
+<RouterProvider router = {router}/>
+
+
+)
