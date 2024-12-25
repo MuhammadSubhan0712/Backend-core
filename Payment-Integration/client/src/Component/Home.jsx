6bb22526a1238bf8410bbeb9efe2d2915eafaa9b
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-26 03:09:48 +0500

    Working...

--- Payment-Integration/client/src/Component/Home.jsx
+++ Payment-Integration/client/src/Component/Home.jsx
@@ -3,23 +3,65 @@ import React from "react";
 const Home = () => {
   const products = [
     {
-      title: "Wireless Bluetooth Headphones",
-      price: 59.99,
-      image:
-        "https://images.pexels.com/photos/14541034/pexels-photo-14541034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      title: "Smartphone 128GB",
-      price: 699.99,
-      image:
-        "https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      title: "Gaming Laptop",
-      price: 1199.99,
-      image:
-        "https://images.pexels.com/photos/19012051/pexels-photo-19012051/free-photo-of-modern-gaming-laptop-and-headphones-on-a-desk.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
+        title: "Wireless Bluetooth Headphones",
+        price: 59.99,
+        image:
+          "https://images.pexels.com/photos/14541034/pexels-photo-14541034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Smartphone 128GB",
+        price: 699.99,
+        image:
+          "https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Gaming Laptop",
+        price: 1199.99,
+        image:
+          "https://images.pexels.com/photos/19012051/pexels-photo-19012051/free-photo-of-modern-gaming-laptop-and-headphones-on-a-desk.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "4K LED Smart TV",
+        price: 399.99,
+        image:
+          "https://images.pexels.com/photos/28884413/pexels-photo-28884413/free-photo-of-modern-home-office-tech-setup-with-gadgets.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Stainless Steel Water Bottle",
+        price: 15.99,
+        image:
+          "https://images.pexels.com/photos/7815021/pexels-photo-7815021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Noise-Cancelling Earbuds",
+        price: 79.99,
+        image:
+          "https://images.pexels.com/photos/9610816/pexels-photo-9610816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Fitness Tracker Watch",
+        price: 49.99,
+        image:
+          "https://plus.unsplash.com/premium_photo-1681433383783-661b519b154a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zml0bmVzcyUyMHRyYWNrZXJ8ZW58MHx8MHx8fDA%3D",
+      },
+      {
+        title: "Portable Speaker",
+        price: 25.99,
+        image:
+          "https://images.pexels.com/photos/19238583/pexels-photo-19238583/free-photo-of-magsafe-iphone-stand-oakywood.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Electric Toothbrush",
+        price: 39.99,
+        image:
+          "https://images.pexels.com/photos/8823972/pexels-photo-8823972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        title: "Gaming Mouse",
+        price: 29.99,
+        image:
+          "https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
   ];
 
   return (
