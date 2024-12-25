f6809b3df1fbcf077466072d11a39cb37380ec9b
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-26 03:37:57 +0500

    Working..

--- Payment-Integration/server/index.js
+++ Payment-Integration/server/index.js
@@ -9,3 +9,27 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello payment integration");
 });
+
+app.post("/api/v1/checkout" , async (req , res) => {
+    const { products } = req.body;
+    const lineItems = products.map((item) => ({
+        price_data: {
+            currency: "usd",
+            product_data: {
+                name: item.name,
+            },
+            unit_amount: item.price * 100,
+        },
+        quantity:item.quantity,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: "http://localhost:5173/success",
+        cancel_url: "http://localhost:5173/cancel",
+      });
+     
+})
+ 
