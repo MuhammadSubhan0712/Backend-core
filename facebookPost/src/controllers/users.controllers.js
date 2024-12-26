0871df68d76e3870d67d0b9d8467cbf03219a415
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-27 01:45:31 +0500

    Working..

---
+++ facebookPost/src/controllers/users.controllers.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+import User from "../models/users.model.js";
+
+const registerUser = async (req ,res ) => {
+    const {fullname , email , password} = req.body;
+
+    if (!fullname || !email || !password) {
+        res.status(400).json({
+            message: "All fields are required",
+        })
+        return;
+    }
+    
+    try {
+        
+        const Existence = await User.findOne({email});
+
+        if (Existence) {
+            res.status(400).json({
+                message:" User Already exist",
+            })
+        }
+
+        const Newuser = await User.create({ fullname , email , password });
+        res.status(200).json({
+            message: "User created Successfully",
+        });    
+    } 
+    catch (error) {
+        res.status(400).json({
+            message:"Error creating user",
+            error,
+        })
+    }
+    
+}
+
