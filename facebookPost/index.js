f2a5e29e4109b10d1d908838d718c8ce7b534c78
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-27 01:34:51 +0500

    Models done..

---
+++ facebookPost/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/db/index.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("HeLlO facebook Posts");
+});
+
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`⚙️ Server is running at the port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB connection failed !!! ", error);
+  });
---
+++ facebookPost/src/models/like.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Posts",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Like", likeSchema);
