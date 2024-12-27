e52be58ab4bef76ee32d26cf58027667335499ba
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-27 22:16:26 +0500

    Working..

--- Node_mailer/index.js
+++ Node_mailer/index.js
@@ -3,3 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("HELLo Nodemailer");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`:) Server is running at the port:${process.env.PORT}`);
+});
