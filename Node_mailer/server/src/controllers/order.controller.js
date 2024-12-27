82c2b55c15d7a3ac377cba85b365b3ea35390c1f
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-28 00:13:31 +0500

    controllers done

--- Node_mailer/server/src/controllers/order.controller.js
+++ Node_mailer/server/src/controllers/order.controller.js
@@ -2,42 +2,44 @@ import nodemailer from "nodemailer";
 import Order from "../models/order.model.js";
 
 // To create order
-export const placeOrder = async (req , res) => {
-    const { userEmail , items , total  } = req.body;
+export const placeOrder = async (req, res) => {
+  const { userEmail, items, total } = req.body;
 
-    if (!userEmail || !items || !total) {
-        res.status(400).json({
-            message:"You must enter all fields i.e: email , items , quantity",
-        });
-        return;
-    }
+  if (!userEmail || !items || !total) {
+    res.status(400).json({
+      message: "You must enter all fields i.e: email , items , quantity",
+    });
+    return;
+  }
 
-    try {
-        const order = await Order.create({userEmail , items , total});
-        await order.save();
+  try {
+    const order = await Order.create({ userEmail, items, total });
+    await order.save();
 
-        // Send Confirmation email
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false,
+    // Send Confirmation email
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
 
-            auth:{
-                user: 'ressie.hermann96@ethereal.email',
-                pass : 'qAEhKNqtUVUgjW1hj8'
-            }
-        });
+      auth: {
+        user: "ressie.hermann96@ethereal.email",
+        pass: "qAEhKNqtUVUgjW1hj8",
+      },
+    });
 
-        await transporter.sendMail({
-            from: '"Asfar the GrEaT 🥰" <ressie.hermann96@ethereal.email>', //Sender Address
-            to : "muhammmadsubhan0712@gmail.com",
-            subject: "Order Confirmation",
-            text: `Your order has been placed succcessfully. Quantity: ${total} \n Items: ${items}`
-        });
+    await transporter.sendMail({
+      from: '"Asfar the GrEaT 🥰" <ressie.hermann96@ethereal.email>', //Sender Address
+      to: "muhammmadsubhan0712@gmail.com",
+      subject: "Order Confirmation",
+      text: `Your order has been placed succcessfully. Quantity: ${total} \n Items: ${items}`,
+      html: "<abbr title=MSubhanKhan>MSK</abbr>", //html body
+    });
 
-        res.status(200).json({message:"Your order has been placed successfully"});
-
-    } catch (error) {
-        res.status(400).json({message:"Error to placed order",error});
-    }
-}
+    res
+      .status(200)
+      .json({ message: "Your order has been placed successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Error to placed order", error });
+  }
+};
