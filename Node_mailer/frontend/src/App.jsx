faf480a28fe5c0ef5d5fc0fb16649d6afcddc912
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-28 01:39:01 +0500

    Working..

--- Node_mailer/frontend/src/App.jsx
+++ Node_mailer/frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="container my-5">
+      <h1 className="text-center mb-4">E-Commerce Store</h1>
+      
+    </div>
+  );
+};
 
-export default App
+export default App;
