cbea89c9718fa24d56c16f4da4c6f41c35fae688
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-31 01:43:29 +0500

    Done.

--- Socket-io/client/app.js
+++ Socket-io/client/app.js
@@ -1,9 +1,29 @@
-import {io} from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js"
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 
-const socket = io('http://localhost:3000');
+const socket = io("http://localhost:3000");
 
+socket.on("connect", () => {
+  console.log("Connected to the server");
+});
 
-socket.on('connect' , () => {
-    console.log('Connected to the server');
-    
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const ul = document.querySelector("ul");
+
+const arr = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  socket.emit("message", input.value);
+  console.log("message send");
+});
+
+socket.on("message", (message) => {
+  arr.push(message);
+  console.log("Message from server", message);
+  ul.innerHTML = "";
+  arr.map((item) => {
+    ul.innerHTML += `<li>📨  ${item}  📨</li>`;
+  });
 });
