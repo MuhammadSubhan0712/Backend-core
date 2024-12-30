ba09e78905f8b50c78adc5361433ed5d84619f49
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-31 00:37:23 +0500

    Working..

---
+++ Pagination/src/routes/course.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { addCourse, getCourse } from "../controllers/course.controllers.js";
+
+const router = express.Router();
+
+// To add course
+router.post("/course", addCourse);
+
+// To get course
+router.get("/courses", getCourse);
+
+
+export default router;
