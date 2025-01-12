5f0f88c886513437fe593b2dcee861f171c33beb
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-13 03:03:21 +0500

    Setup Complete

--- Social-Media Post/frontend/src/main.jsx
+++ Social-Media Post/frontend/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Register from "./Pages/Register.jsx";
+import Login from "./Pages/Login.jsx";
+import Posts from "./Pages/Posts.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/post",
+        element: <Posts />,
+      },
+      {
+        path: "*",
+        element: (
+          <h1 className="p-3 mt-5 text-xl flex justify-center items-center text-red-600 font-semibold">
+            {" "}
+            404 | Not Found{" "}
+          </h1>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
