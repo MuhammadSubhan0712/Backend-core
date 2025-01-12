33159f90fa577f8d8167e0a8d10266f8217c98dc
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-13 02:16:35 +0500

    user routes

--- Social-Media Post/server/index.js
+++ Social-Media Post/server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDB from "./src/db/index.js";
 import userRouter from "./src/routes/user.route.js";
 import postRouter from "./src/routes/post.route.js";
@@ -12,8 +13,10 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("HeLlO Social Posts");
+  res.send("HeLlO Social Media Posts");
 });
+
+cookieParser();
 
 app.use("/user" , userRouter);
 app.use("/post" , postRouter);
@@ -22,7 +25,7 @@ app.use("/post" , postRouter);
 connectDB()
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`⚙️ Server is running at the port ${process.env.PORT}`);
+      console.log(`⚙️   Server is running at the port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
--- Social-Media Post/server/src/routes/user.route.js
+++ Social-Media Post/server/src/routes/user.route.js
@@ -1,10 +1,24 @@
 import express from "express";
 
-import {registerUser} from "../controllers/users.controllers.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshToken,
+} from "../controllers/users.controllers.js";
 
 const router = express.Router();
 
 // To create User
-router.post("/register" , registerUser);
+router.post("/register", registerUser);
+
+// To login User
+router.post("/login", loginUser);
+
+// To logout User
+router.get("/logout", logoutUser);
+
+// To refresh token
+router.post("/refreshtoken", refreshToken);
 
 export default router;
