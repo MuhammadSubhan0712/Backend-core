7bda85d429225f219fdb7cbab63d59f62764ee58
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-14 02:31:52 +0500

    Working..

--- Social-Media Post/frontend/src/Pages/Register.jsx
+++ Social-Media Post/frontend/src/Pages/Register.jsx
@@ -51,6 +51,7 @@ const Register = () => {
       <div className="mt-8 flex space-x-4">
       <span className="h-4 w-4 bg-green-400 rounded-full animate-pulse"></span>
       <span className="h-4 w-4 bg-yellow-400 rounded-full animate-pulse delay-75"></span>
+      <span className="h-4 w-4 bg-green-600 rounded-full animate-pulse delay-150"></span>
 
       </div>
     </>
