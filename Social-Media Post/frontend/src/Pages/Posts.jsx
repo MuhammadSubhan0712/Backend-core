3651062625988c1a53ae07bc9c2baf16df0373c0
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-01-14 21:30:09 +0500

    Working..

--- Social-Media Post/frontend/src/Pages/Posts.jsx
+++ Social-Media Post/frontend/src/Pages/Posts.jsx
@@ -14,26 +14,25 @@ const Posting = () => {
 
       {/* Form Container */}
       <div className="bg-[#121212] p-8 rounded-3xl shadow-2xl w-full max-w-md transform hover:scale-105 transition-transform">
-        <form className="space-y-6">
-    
+      <form className="space-y-6">
           {/* Post Title Input */}
           <div>
             <Input
               type="text"
               placeholder="Title"
-              className="w-full bg-zinc-700 text-white placeholder-gray-400 focus:ring-2 focus:ring-yellow-400 focus:outline-none rounded-lg p-3 transition-shadow"
+              className="w-full bg-zinc-800 text-white placeholder-gray-400 focus:ring-4 focus:ring-yellow-400 focus:outline-none rounded-lg p-4 shadow-lg"
             />
           </div>
 
           {/* Post Image Input */}
-          {/* <div>
+          <div>
             <Input
               type="file"
-              value={}
-              className="w-full bg-zinc-700 text-white placeholder-gray-400 focus:ring-2 focus:ring-yellow-400 focus:outline-none rounded-lg p-3 transition-shadow"
+              className="w-full bg-zinc-800 text-white placeholder-gray-400 focus:ring-4 focus:ring-yellow-400 focus:outline-none rounded-lg p-4 shadow-lg"
             />
-          </div> */}
+          </div>
 
+          
           {/* Post Button */}
           <Button className="w-full bg-gradient-to-r from-yellow-400 via-green-400 to-yellow-500 text-gray-900 font-bold py-3 rounded-lg hover:scale-105 hover:shadow-lg transition-transform">
           Post
