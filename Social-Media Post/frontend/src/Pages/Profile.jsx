0b732e406947db222f56ba87c26ede515e175700
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-16 20:52:42 +0500

    Working.

--- Social-Media Post/frontend/src/Pages/Profile.jsx
+++ Social-Media Post/frontend/src/Pages/Profile.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useAuth } from "../context/AuthContext";
+import axios from 'axios';
+import { toast } from 'sonner';
 const Profile = () => {
+    const { user, logout } = useAuth();
+    const [formData, setFormData] = useState({
+        username:"",
+        email:"",
+        password:"",
+    });
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() =>{ 
+        if (user) {
+            setFormData({
+                username: user.username,
+                email: user.email,
+                bio: user.bio || '',
+            });
+            fetchUserPosts();
+        }
+    },[]);
+
+    const fetchUserPosts = async () => {
+        try {
+            const response = await axios.get(`/api/posts/user/${user._id}`);
+            setPosts(response.data);
+        } catch (error) {
+            toast.error("Failed to fetch posts", error);
+        }
+    };
+    const handleUpdate = async (event) => {
+       
+    }
   return (
     <div>Profile</div>
   )
