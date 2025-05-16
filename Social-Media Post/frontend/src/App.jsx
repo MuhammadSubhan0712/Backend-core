e372c95e65273be9881a2a2b63d85e2afa698b46
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2025-05-16 20:44:03 +0500

    Working.

--- Social-Media Post/frontend/src/App.jsx
+++ Social-Media Post/frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-import Layout from './Layout'
-
+import { AuthProvider } from './context/AuthContext'
+import { Toaster } from 'sonner'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import Profile from './Pages/Profile';
 const App = () => {
+
   return (
-    <Layout/>
+    <>
+    <AuthProvider/>
+    <Toaster richColors position='top-center' />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={
+        <ProtectedRoute>
+          
+        </ProtectedRoute>
+      }/>
+      <Route path='/profile' element={
+        <ProtectedRoute>
+          <Profile/>
+        </ProtectedRoute>
+      } />
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
--- Social-Media Post/frontend/src/main.jsx
+++ Social-Media Post/frontend/src/main.jsx
@@ -1,41 +1,11 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Layout.jsx";
-import Register from "./Pages/Register.jsx";
-import Login from "./Pages/Login.jsx";
-import Posting from "./Pages/Posts.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/post",
-        element: <Posting />,
-      },
-      {
-        path: "*",
-        element: (
-          <h1 className="p-3 mt-5 text-3xl flex justify-center items-center text-red-600 font-semibold">
-            {" "}
-            404 | Not Found{" "}
-          </h1>
-        ),
-      },
-    ],
-  },
-]);
 
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
